54c3df0ad24660bf1d53fa08b9540132c055ed4d
Mace <mason9701s@gmail.com>
2021-01-25 22:17:45 -0600

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,10 +2,27 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
+
+    /**
+     * Returns the next number in a Collatz sequence, after input n
+     * @param n
+     * @return
+     */
+    public static int nextNumber(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return 3 * n + 1;
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
-
+        do {
+            System.out.print(n + " ");
+            n = nextNumber(n);
+        } while (n > 1);
+        System.out.print(n);
     }
 }
 
--- lab1/GetEnvironmentVariables.java
+++ lab1/GetEnvironmentVariables.java
@@ -3,7 +3,7 @@ import java.util.Properties;
 public class GetEnvironmentVariables {
 
     //  Fill in the path to your sp21-s*** folder between the quotes
-    public static String REPO_DIR = "";
+    public static String REPO_DIR = "/Users/xiaoyusun/ucb/Berkeley-CS61B-Spring2021";
 
     //  Fill in the path to your snaps-sp21-s*** folder between the quotes
     public static String SNAPS_DIR = "";

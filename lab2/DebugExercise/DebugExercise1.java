d1e5d5355f6afaac9ea680d696e70a1a3151871e
Mace <mason9701s@gmail.com>
2021-02-01 20:43:25 -0600

    completed DebugExercise1

--- lab2/DebugExercise/DebugExercise1.java
+++ lab2/DebugExercise/DebugExercise1.java
@@ -6,7 +6,7 @@ package DebugExercise;
  */
 public class DebugExercise1 {
     public static int divideThenRound(int top, int bottom) {
-        int quotient = top / bottom;
+        float quotient = (float) (top * 1.0 / bottom);
         int result = Math.round(quotient);
         return result;
     }

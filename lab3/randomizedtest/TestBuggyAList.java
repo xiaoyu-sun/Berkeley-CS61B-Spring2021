f02b655f80e4d275d518a0eeaaa650371a7c0607
Mace <mason9701s@gmail.com>
2021-02-14 20:22:00 -0600

    doing randomizedTest

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,53 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        correct.addLast(5);
+        correct.addLast(10);
+        correct.addLast(15);
+
+        broken.addLast(5);
+        broken.addLast(10);
+        broken.addLast(15);
+
+        assertEquals(correct.size(), broken.size());
+
+        assertEquals(correct.removeLast(), broken.removeLast());
+        assertEquals(correct.removeLast(), broken.removeLast());
+        assertEquals(correct.removeLast(), broken.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addLast(randVal);
+                broken.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                assertEquals(correct.size(), broken.size());
+//                System.out.println("size: " + size);
+            } else if (operationNumber == 2 && correct.size() > 0 && broken.size() > 0) {
+                // getLast
+                assertEquals(correct.getLast(), broken.getLast());
+//                System.out.println("getLast(): " + last);
+            } else if (operationNumber == 3 && correct.size() > 0 && broken.size() > 0) {
+                // removeLast
+                assertEquals(correct.removeLast(), broken.removeLast());
+//                System.out.println("removeLast(): " + last);
+            }
+        }
+    }
 }

c69dfae12d068de2f2efc11e3d263cdadf9811b1
Mace <mason9701s@gmail.com>
2021-02-16 08:17:28 -0600

    addFirst and addLast

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -22,4 +22,36 @@ public class LinkedListDeque<T> {
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
     }
+
+    public void addFirst(T item) {
+        Node newNode = new Node(item, sentinel, sentinel.next);
+        // The "prev" of the original first node should now point to new first node
+        sentinel.next.prev = newNode;
+        // The "next" of sentinel node should also point to new first node
+        sentinel.next = newNode;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        Node newNode = new Node(item, sentinel.prev, sentinel);
+        // The "next" of the original last node should now point to new last node
+        sentinel.prev.next = newNode;
+        // The "prev" of sentinel node should also point to new last node
+        sentinel.prev = newNode;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        System.out.println("Mason");
+    }
+
+
 }

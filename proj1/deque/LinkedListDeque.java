460e2a125c41d2e35bcaddd6242ba41d87189986
Mace <mason9701s@gmail.com>
2021-02-15 22:41:27 -0600

    checkpoint 210215

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,25 @@
+package deque;
+
+public class LinkedListDeque<T> {
+
+    public class Node {
+        public T item;
+        public Node prev;
+        public Node next;
+        public Node(T item, Node prev, Node next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private int size;
+    private Node sentinel;
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+    }
+}

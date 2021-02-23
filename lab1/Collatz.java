2ad1d01be880a9dc2589380cbdd248221e3066ea
Mace <mason9701s@gmail.com>
2021-02-22 20:44:35 -0600

    finished lab4

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,45 +3,26 @@
  */
 public class Collatz {
 
-
     /**
      * Returns the next number in a Collatz sequence, after input n
      * @param n
      * @return
      */
-//    public static int nextNumber(int n) {
-//        if (n % 2 == 0) {
-//            return n / 2;
-//        } else {
-//            return 3 * n + 1;
-
-    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
-
+            return 3 * n + 1;
         }
     }
 
     public static void main(String[] args) {
         int n = 5;
-//        do {
-//            System.out.print(n + " ");
-//            n = nextNumber(n);
-//        } while (n > 1);
-//        System.out.print(n);
-
-        System.out.print(n + " ");
-        while (n != 1) {
-            n = nextNumber(n);
+        do {
             System.out.print(n + " ");
-        }
-        System.out.println();
-
+            n = nextNumber(n);
+        } while (n > 1);
+        System.out.print(n);
     }
 }
 
